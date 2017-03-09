bffe12ed86a9fa2866fa12750d7ff3ad8cb2e968
Richard Barlow <richard@richardbarlow.co.uk>
2017-03-09 20:27:53 +0000

    Add arg to print_label.py to add more separation
    This allows the two parts to be separated with scissors.

--- print_label.py
+++ print_label.py
@@ -11,11 +11,14 @@ def get_args():
     parser = argparse.ArgumentParser("Generate and print barcode label")
     parser.add_argument("partcode")
     parser.add_argument("-n", action="store_true", help="Don't print label")
+    parser.add_argument("-s", action="store_true", help="Separate barcode and "
+                        "'property of' to allow them to be cut up")
     return parser.parse_args()
 
 
-def generate_label(part_code):
+def generate_label(part_code, separate):
     left_margin = 32
+    msg_gap = 20 if separate else 0
 
     ocr_font = ImageFont.truetype(file_dir + '/OCRA.ttf', 32)
     msg_font = ImageFont.truetype('/usr/share/fonts/dejavu/DejaVuSans-Bold.ttf', 16)
@@ -23,7 +26,7 @@ def generate_label(part_code):
     barcode_image = code128.image(part_code, thickness=2, height=32, quiet_zone=False)
     barcode_width, barcode_height = barcode_image.size
 
-    label_width = barcode_width+left_margin+113
+    label_width = barcode_width+left_margin+113+msg_gap
     label_image = Image.new('L', (label_width, 64), 255)
     label_image.paste(barcode_image, (left_margin, 0))
     draw = ImageDraw.Draw(label_image)
@@ -32,9 +35,10 @@ def generate_label(part_code):
     part_code_text_position = (((barcode_width-text_width)/2)+left_margin, 34)
     draw.text(part_code_text_position, part_code, font=ocr_font)
 
-    msg1_position = (barcode_width + left_margin + 10, 3)
-    msg2_position = (barcode_width + left_margin + 23, 22)
-    msg3_position = (barcode_width + left_margin + 20, 40)
+    msg_start_position = barcode_width + left_margin + msg_gap
+    msg1_position = (msg_start_position + 10, 3)
+    msg2_position = (msg_start_position + 23, 22)
+    msg3_position = (msg_start_position + 20, 40)
     draw.text(msg1_position, "Property of", font=msg_font)
     draw.text(msg2_position, "Student", font=msg_font)
     draw.text(msg3_position, "Robotics", font=msg_font)
@@ -54,7 +58,7 @@ def print_label(f, wpx, hpx):
 if __name__ == "__main__":
     args = get_args()
     partcode = args.partcode.upper()
-    label = generate_label(partcode)
+    label = generate_label(partcode, args.s)
     label_filename = "{0}.png".format(partcode)
     label.save(label_filename)
 
